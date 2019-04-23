d0f580aead04517d3b793f8a0e5c3582b35a88cb
alexxuss1996 <alekseymelnych132@gmail.com>
2019-04-23 18:50:09 +0300

    fix wrong import in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import "semantic-ui-css/themes/default/assets/images/";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
