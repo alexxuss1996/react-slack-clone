8fed316ddfc0dbeefeecf8d2c19b4381d0b26946
alexxuss1996 <alekseymelnych132@gmail.com>
2019-05-10 19:41:04 +0300

    Add message to messages db collection

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
 import { Button, Input, Segment } from "semantic-ui-react";
+import Firebase from "firebase/app";
 
 export default class MessageForm extends Component {
+  state = {
+    message: "",
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    loading: false,
+    errors: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  createMessage = () => {
+    const { user } = this.state;
+    const message = {
+      timestamp: Firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        avatar: user.photoURL
+      },
+      content: this.state.message
+    };
+    return message;
+  };
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message, channel, errors } = this.state;
+
+    if (message) {
+      this.setState({
+        loading: true
+      });
+      messagesRef
+        .child(channel.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => this.setState({ loading: false, message: "", errors: [] }))
+        .catch(err => {
+          console.error(err);
+          this.setState({ loading: false, errors: errors.concat(err) });
+        });
+    } else {
+      this.setState({
+        errors: errors.concat({ message: "Add a message" })
+      });
+    }
+  };
   render() {
+    const { errors } = this.state;
     return (
       <Segment className="message__form">
         <Input
@@ -11,10 +64,12 @@ export default class MessageForm extends Component {
           style={{ marginBottom: "0.7em" }}
           label={<Button icon={"add"} />}
           labelPosition="left"
+          onChange={this.handleChange}
+          className={errors.some(error => error.message.includes("message")) ? "error" : ""}
           placeholder="Write your message"
         />
         <Button.Group icon widths="2">
-          <Button color="orange" content="Add Reply" labelPosition="left" icon="edit" />
+          <Button color="orange" content="Add Reply" labelPosition="left" onClick={this.sendMessage} icon="edit" />
           <Button color="teal" content="Upload Media" labelPosition="right" icon="cloud upload" />
         </Button.Group>
       </Segment>
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 
 export default class Messages extends Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
         <Segment>
           <Comment.Group className="messages">{/* Messages */}</Comment.Group>
         </Segment>
-        <MessageForm />
+        <MessageForm messagesRef={messagesRef} currentUser={user} currentChannel={channel} />
       </React.Fragment>
     );
   }
